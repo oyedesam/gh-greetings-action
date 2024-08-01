639a784700a72bda8c5121acae7e3a1f235e2a10
oyedesam <samson.oyede@lexisnexisrisk.com>
2024-08-01 17:42:13 +0100

    First action

---
+++ main.js
@@ -0,0 +1,21 @@
+const { core } = require('@actions/core');
+const github = require('@actions/github');
+const { exec } = require('@actions/exec');
+
+
+var name = core.getInput('name');
+
+function run(name) {
+    core.debug('Starting my custom action....');
+
+    if (!name) {
+        core.error(`The name was not set and it is required`);
+    }
+    core.info(`The name is ${name}`);
+    var time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    
+}
+
+
+run(name);
